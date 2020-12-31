42fb8b45376439378a5f1014401fbd1125968c23
gfvxgd2k <gfvxgd2k@naver.com>
2021-01-01 03:20:22 +0900

    add seq2seq class

--- RNN/seq2seq.py
+++ RNN/seq2seq.py
@@ -1,4 +1,5 @@
 #출저: https://github.com/ndb796/Deep-Learning-Paper-Review-and-Practice/blob/master/code_practices/Sequence_to_Sequence_with_LSTM_Tutorial.ipynb
+from numpy import random
 from torch.nn.modules import dropout
 import spacy
 from torchtext.data import Field, BucketIterator
@@ -59,12 +60,13 @@ def tokenize_en(text):
 class Encoder(nn.Module):
     def __init__(self, input_dim, embed_dim, hidden_dim, n_layers, dropout_ratio):
         super().__init__()
-
+        #nn.Embedding RNN/Embedding_ex.py 참조
         self.embedding = nn.Embedding(input_dim, embed_dim)
         #lstm
         self.hidden_Dim = hidden_dim
         self.n_layers = n_layers
         self.rnn = nn.LSTM(embed_dim, hidden_dim, n_layers, dropout=dropout_ratio)
+
         self.dropout = nn.Dropout(dropout_ratio)
 
     def fowrard(self, src):
@@ -73,6 +75,67 @@ class Encoder(nn.Module):
         outputs, (hidden, cell)= self.rnn(embedded)
 
         return hidden, cell
+
+class Decoder(nn.Module):
+    def __init__(self, output_dim, embed_dim, hidden_dim, n_layers, dropout_ratio):
+        super().__init__()
+
+        self.embedding = nn.Embedding(output_dim, embed_dim)
+
+        #lstm
+        self.hidden_Dim = hidden_dim
+        self.n_layers = n_layers
+        self.rnn = nn.LSTM(embed_dim, hidden_dim, n_layers, dropout=dropout_ratio)
+
+        self.output_dim = output_dim
+        self.fc_out = nn.Linear(hidden_dim, output_dim)
+
+        self.dropout = nn.Dropout(dropout_ratio)
+
+    def forward(self, input, hidden, cell):
+        input = input.unsqueeze(0)
+        # input: [단어 개수 = 1, 배치 크기]
+
+        embedded = self.dropout(self.embedding(input))
+        # embedded: [단어 개수, 배치 크기, 임베딩 차원]
+
+        output, (hidden, cell) = self.rnn(embedded, (hidden, cell))
+        # output: [단어 개수 = 1, 배치 크기, 히든 차원]: 현재 단어의 출력 정보
+        # hidden: [레이어 개수, 배치 크기, 히든 차원]: 현재까지의 모든 단어의 정보
+        # cell: [레이어 개수, 배치 크기, 히든 차원]: 현재까지의 모든 단어의 정보
+
+        prediction = self.fc_out(output.squeeze(0))
+        # prediction = [배치 크기, 출력 차원]
+        return prediction, hidden, cell
+
+class Seq2Seq(nn.Module):
+    def __init__(self, encoder, decoder, device):
+        super().__init__()
+
+        self.encoder = encoder
+        self.decoder = decoder
+        self.device = device
+
+    def forward(self, src, trg, teacher_forcing_ratio = 0.5):
+        hidden, cell = self.encoder(src)
+
+        trg_len = trg.shape[0]
+        batch_size = trg.shape[1]
+        trg_vocab_size = self.decoder.output_dim
+        outputs = torch.zeros(trg_len, batch_size, trg_vocab_size).to(self.device)
+
+        input = trg[0, :]
+
+        for t in range(1,trg_len):
+            output, hidden, cell = self.decoder(input, hidden, cell)
+
+            outputs[t] = output
+            top1 = output.argmax(1)
+
+            teacher_force = random.random() < teacher_forcing_ratio
+            input = trg[t] if teacher_force else top1
+
+        return outputs
 
 if __name__ == '__main__':
     main()
